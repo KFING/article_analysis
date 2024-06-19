2c14deaf3c94bf82cd8b6eeaf48320c3f437f00c
Ehor Meschenko <meshhenko04@bk.ru>
2024-06-19 09:56:19 +0300

    add upgrade for keycloak

--- article-analysis/settings.py
+++ article-analysis/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject.urls'
+ROOT_URLCONF = 'article-analysis.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djangoProject.wsgi.application'
+WSGI_APPLICATION = 'article-analysis.wsgi.application'
 
 
 # Database
